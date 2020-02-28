d011144e90e2906f96598d7c96c5c2246655aa98
DoomMetal <tacticaldoommetal@gmail.com>
2020-02-28 19:27:58 +0100

    Updated recruit squad dialog

--- AntistasiGuiPrototype.Stratis/GUI/recruitSquadDialog.hpp
+++ AntistasiGuiPrototype.Stratis/GUI/recruitSquadDialog.hpp
@@ -41,84 +41,356 @@ class A3A_RecruitSquadDialog
 
       class controls
       {
+        class InfSquadIcon : RscPicture
+        {
+          idc = -1;
+          colorBackground[] = {0,0,0,0};
+          colorText[]= {0.7,0.7,0.7,1};
+          text = "GUI\textures\icon_none.paa";
+          x = 24 * GRID_W;
+          y = 13 * GRID_H;
+          w = 8 * GRID_W;
+          h = 8 * GRID_H;
+        };
+
+        class InfSquadPriceText : RscText
+        {
+          idc = -1;
+          style = ST_CENTER;
+          text = "€ 5000";
+          sizeEx = GUI_TEXT_SIZE_MEDIUM;
+          shadow = 2;
+          x = 24 * GRID_W;
+          y = 19 * GRID_H;
+          w = 8 * GRID_W;
+          h = 4 * GRID_H;
+        };
+
         class InfSquadButton : A3A_ShortcutButton
         {
           idc = -1;
           text = "Inf. Squad";
-          x = 16 * GRID_W;
-          y = 13 * GRID_H;
-          w = 32 * GRID_W;
+          size = GUI_TEXT_SIZE_LARGE;
+          x = 36 * GRID_W;
+          y = 11 * GRID_H;
+          w = 36 * GRID_W;
+          h = 12 * GRID_H;
+
+          class TextPos
+          {
+            left = 2 * GRID_W;
+            right = 2 * GRID_W;
+            top = 3 * GRID_H;
+            bottom = 3 * GRID_W;
+          };
+        };
+
+        class InfTeamIcon : RscPicture
+        {
+          idc = -1;
+          colorBackground[] = {0,0,0,0};
+          colorText[]= {0.7,0.7,0.7,1};
+          text = "GUI\textures\icon_none.paa";
+          x = 24 * GRID_W;
+          y = 34 * GRID_H;
+          w = 8 * GRID_W;
           h = 8 * GRID_H;
+        };
+
+        class InfTeamPriceText : RscText
+        {
+          idc = -1;
+          style = ST_CENTER;
+          text = "€ 5000";
+          sizeEx = GUI_TEXT_SIZE_MEDIUM;
+          shadow = 2;
+          x = 24 * GRID_W;
+          y = 40 * GRID_H;
+          w = 8 * GRID_W;
+          h = 4 * GRID_H;
         };
 
         class InfTeamButton : A3A_ShortcutButton
         {
           idc = -1;
           text = "Inf. Team";
-          x = 16 * GRID_W;
-          y = 34 * GRID_H;
-          w = 32 * GRID_W;
-          h = 8 * GRID_H;
+          size = GUI_TEXT_SIZE_LARGE;
+          x = 36 * GRID_W;
+          y = 32 * GRID_H;
+          w = 36 * GRID_W;
+          h = 12 * GRID_H;
+
+          class TextPos
+          {
+            left = 2 * GRID_W;
+            right = 2 * GRID_W;
+            top = 3 * GRID_H;
+            bottom = 3 * GRID_W;
+          };
         };
 
-        class AtTeamButton : A3A_ShortcutButton
+        class MgTeamIcon : RscPicture
         {
           idc = -1;
-          text = "AT Team";
-          x = 16 * GRID_W;
+          colorBackground[] = {0,0,0,0};
+          colorText[]= {0.7,0.7,0.7,1};
+          text = "GUI\textures\icon_none.paa";
+          x = 24 * GRID_W;
           y = 55 * GRID_H;
-          w = 32 * GRID_W;
+          w = 8 * GRID_W;
           h = 8 * GRID_H;
         };
 
-        class SniperTeamButton : A3A_ShortcutButton
+        class MgTeamPriceText : RscText
         {
           idc = -1;
-          text = "Sniper Team";
-          x = 16 * GRID_W;
-          y = 76 * GRID_H;
-          w = 32 * GRID_W;
-          h = 8 * GRID_H;
+          style = ST_CENTER;
+          text = "€ 5000";
+          sizeEx = GUI_TEXT_SIZE_MEDIUM;
+          shadow = 2;
+          x = 24 * GRID_W;
+          y = 61 * GRID_H;
+          w = 8 * GRID_W;
+          h = 4 * GRID_H;
         };
 
         class MgTeamButton : A3A_ShortcutButton
         {
           idc = -1;
           text = "MG Team";
-          x = 112 * GRID_W;
-          y = 13 * GRID_H;
-          w = 32 * GRID_W;
+          size = GUI_TEXT_SIZE_LARGE;
+          x = 36 * GRID_W;
+          y = 53 * GRID_H;
+          w = 36 * GRID_W;
+          h = 12 * GRID_H;
+
+          class TextPos
+          {
+            left = 2 * GRID_W;
+            right = 2 * GRID_W;
+            top = 3 * GRID_H;
+            bottom = 3 * GRID_W;
+          };
+        };
+
+        class AtTeamIcon : RscPicture
+        {
+          idc = -1;
+          colorBackground[] = {0,0,0,0};
+          colorText[]= {0.7,0.7,0.7,1};
+          text = "GUI\textures\icon_none.paa";
+          x = 24 * GRID_W;
+          y = 76 * GRID_H;
+          w = 8 * GRID_W;
           h = 8 * GRID_H;
+        };
+
+        class AtTeamPriceText : RscText
+        {
+          idc = -1;
+          style = ST_CENTER;
+          text = "€ 5000";
+          sizeEx = GUI_TEXT_SIZE_MEDIUM;
+          shadow = 2;
+          x = 24 * GRID_W;
+          y = 82 * GRID_H;
+          w = 8 * GRID_W;
+          h = 4 * GRID_H;
+        };
+
+        class AtTeamButton : A3A_ShortcutButton
+        {
+          idc = -1;
+          text = "AT Team";
+          size = GUI_TEXT_SIZE_LARGE;
+          x = 36 * GRID_W;
+          y = 74 * GRID_H;
+          w = 36 * GRID_W;
+          h = 12 * GRID_H;
+
+          class TextPos
+          {
+            left = 2 * GRID_W;
+            right = 2 * GRID_W;
+            top = 3 * GRID_H;
+            bottom = 3 * GRID_W;
+          };
+        };
+
+        class MortarTeamIcon : RscPicture
+        {
+          idc = -1;
+          colorBackground[] = {0,0,0,0};
+          colorText[]= {0.7,0.7,0.7,1};
+          text = "GUI\textures\icon_none.paa";
+          x = 128 * GRID_W;
+          y = 13 * GRID_H;
+          w = 8 * GRID_W;
+          h = 8 * GRID_H;
+        };
+
+        class MortarTeamPriceText : RscText
+        {
+          idc = -1;
+          style = ST_CENTER;
+          text = "€ 5000";
+          sizeEx = GUI_TEXT_SIZE_MEDIUM;
+          shadow = 2;
+          x = 128 * GRID_W;
+          y = 19 * GRID_H;
+          w = 8 * GRID_W;
+          h = 4 * GRID_H;
         };
 
         class MortarTeamButton : A3A_ShortcutButton
         {
           idc = -1;
           text = "Mortar Team";
-          x = 112 * GRID_W;
+          size = GUI_TEXT_SIZE_LARGE;
+          x = 88 * GRID_W;
+          y = 11 * GRID_H;
+          w = 36 * GRID_W;
+          h = 12 * GRID_H;
+
+          class TextPos
+          {
+            left = 2 * GRID_W;
+            right = 2 * GRID_W;
+            top = 3 * GRID_H;
+            bottom = 3 * GRID_W;
+          };
+        };
+
+        class SniperTeamIcon : RscPicture
+        {
+          idc = -1;
+          colorBackground[] = {0,0,0,0};
+          colorText[]= {0.7,0.7,0.7,1};
+          text = "GUI\textures\icon_none.paa";
+          x = 128 * GRID_W;
           y = 34 * GRID_H;
-          w = 32 * GRID_W;
+          w = 8 * GRID_W;
           h = 8 * GRID_H;
+        };
+
+        class SniperTeamPriceText : RscText
+        {
+          idc = -1;
+          style = ST_CENTER;
+          text = "€ 5000";
+          sizeEx = GUI_TEXT_SIZE_MEDIUM;
+          shadow = 2;
+          x = 128 * GRID_W;
+          y = 40 * GRID_H;
+          w = 8 * GRID_W;
+          h = 4 * GRID_H;
+        };
+
+        class SniperTeamButton : A3A_ShortcutButton
+        {
+          idc = -1;
+          text = "Sniper Team";
+          size = GUI_TEXT_SIZE_LARGE;
+          x = 88 * GRID_W;
+          y = 32 * GRID_H;
+          w = 36 * GRID_W;
+          h = 12 * GRID_H;
+
+          class TextPos
+          {
+            left = 2 * GRID_W;
+            right = 2 * GRID_W;
+            top = 3 * GRID_H;
+            bottom = 3 * GRID_W;
+          };
+        };
+
+        class AtCarIcon : RscPicture
+        {
+          idc = -1;
+          colorBackground[] = {0,0,0,0};
+          colorText[]= {0.7,0.7,0.7,1};
+          text = "GUI\textures\icon_none.paa";
+          x = 128 * GRID_W;
+          y = 55 * GRID_H;
+          w = 8 * GRID_W;
+          h = 8 * GRID_H;
+        };
+
+        class AtCarPriceText : RscText
+        {
+          idc = -1;
+          style = ST_CENTER;
+          text = "€ 5000";
+          sizeEx = GUI_TEXT_SIZE_MEDIUM;
+          shadow = 2;
+          x = 128 * GRID_W;
+          y = 61 * GRID_H;
+          w = 8 * GRID_W;
+          h = 4 * GRID_H;
         };
 
         class AtCarButton : A3A_ShortcutButton
         {
           idc = -1;
           text = "AT Car";
-          x = 112 * GRID_W;
-          y = 55 * GRID_H;
-          w = 32 * GRID_W;
+          size = GUI_TEXT_SIZE_LARGE;
+          x = 88 * GRID_W;
+          y = 53 * GRID_H;
+          w = 36 * GRID_W;
+          h = 12 * GRID_H;
+
+          class TextPos
+          {
+            left = 2 * GRID_W;
+            right = 2 * GRID_W;
+            top = 3 * GRID_H;
+            bottom = 3 * GRID_W;
+          };
+        };
+
+        class AaTruckIcon : RscPicture
+        {
+          idc = -1;
+          colorBackground[] = {0,0,0,0};
+          colorText[]= {0.7,0.7,0.7,1};
+          text = "GUI\textures\icon_none.paa";
+          x = 128 * GRID_W;
+          y = 76 * GRID_H;
+          w = 8 * GRID_W;
           h = 8 * GRID_H;
+        };
+
+        class AaTruckPriceText : RscText
+        {
+          idc = -1;
+          style = ST_CENTER;
+          text = "€ 5000";
+          sizeEx = GUI_TEXT_SIZE_MEDIUM;
+          shadow = 2;
+          x = 128 * GRID_W;
+          y = 82 * GRID_H;
+          w = 8 * GRID_W;
+          h = 4 * GRID_H;
         };
 
         class AaTruckButton : A3A_ShortcutButton
         {
           idc = -1;
           text = "AA Truck";
-          x = 112 * GRID_W;
-          y = 76 * GRID_H;
-          w = 32 * GRID_W;
-          h = 8 * GRID_H;
+          size = GUI_TEXT_SIZE_LARGE;
+          x = 88 * GRID_W;
+          y = 74 * GRID_H;
+          w = 36 * GRID_W;
+          h = 12 * GRID_H;
+
+          class TextPos
+          {
+            left = 2 * GRID_W;
+            right = 2 * GRID_W;
+            top = 3 * GRID_H;
+            bottom = 3 * GRID_W;
+          };
         };
       };
     };
