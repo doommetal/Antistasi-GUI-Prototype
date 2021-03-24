3333905e2caf907c70df6730699bd5d4e2dd7ed6
DoomMetal <tacticaldoommetal@gmail.com>
2021-03-24 12:46:37 +0100

    updated and localized mission request dialog

--- AntistasiGuiPrototype.Stratis/GUI/requestMissionDialog.hpp
+++ AntistasiGuiPrototype.Stratis/GUI/requestMissionDialog.hpp
@@ -7,7 +7,7 @@ class A3A_RequestMissionDialog : A3A_DefaultDialog
     class TitlebarText : A3A_Text
     {
       idc = -1;
-      text = "Request Mission";
+      text = $STR_antistasi_dialogs_mission_request_titlebar;
       x = DIALOG_X;
       y = DIALOG_Y - 4 * GRID_H;
       w = DIALOG_W * GRID_W;
@@ -21,78 +21,14 @@ class A3A_RequestMissionDialog : A3A_DefaultDialog
 
       class Controls
       {
-        class AssassinationIcon : A3A_Picture
-        {
-          idc = -1;
-          text = A3A_Tex_Icon_Assassination;
-          x = 24 * GRID_W;
-          y = 10 * GRID_H;
-          w = 16 * GRID_W;
-          h = 16 * GRID_H;
-        };
-
-        class AssassinationButton : A3A_Button
-        {
-          idc = -1;
-          text = "Assassination"; // TODO: localize
-          sizeEx = GUI_TEXT_SIZE_LARGE;
-          x = 16 * GRID_W;
-          y = 30 * GRID_H;
-          w = 32 * GRID_W;
-          h = 12 * GRID_H;
-        };
-
-        class LogisticsIcon : A3A_Picture
-        {
-          idc = -1;
-          colorBackground[] = {0,0,0,0};
-          text = A3A_Tex_Icon_Logistics;
-          x = 72 * GRID_W;
-          y = 10 * GRID_H;
-          w = 16 * GRID_W;
-          h = 16 * GRID_H;
-        };
-
-        class LogisticsButton : A3A_Button
-        {
-          idc = -1;
-          text = "Logistics"; // TODO: localize
-          sizeEx = GUI_TEXT_SIZE_LARGE;
-          x = 64 * GRID_W;
-          y = 30 * GRID_H;
-          w = 32 * GRID_W;
-          h = 12 * GRID_H;
-        };
-
-        class ConvoyIcon : A3A_Picture
-        {
-          idc = -1;
-          colorBackground[] = {0,0,0,0};
-          text = A3A_Tex_Icon_Convoy_Ambush;
-          x = 120 * GRID_W;
-          y = 10 * GRID_H;
-          w = 16 * GRID_W;
-          h = 16 * GRID_H;
-        };
-
-        class ConvoyButton : A3A_Button
-        {
-          idc = -1;
-          text = "Convoy Ambush"; // TODO: localize
-          sizeEx = GUI_TEXT_SIZE_LARGE;
-          x = 112 * GRID_W;
-          y = 30 * GRID_H;
-          w = 32 * GRID_W;
-          h = 12 * GRID_H;
-        };
-
+        // Conquest
         class ConquestIcon : A3A_Picture
         {
           idc = -1;
           colorBackground[] = {0,0,0,0};
           text = A3A_Tex_Icon_Conquest;
-          x = 24 * GRID_W;
-          y = 50 * GRID_H;
+          x = 15 * GRID_W;
+          y = 14 * GRID_H;
           w = 16 * GRID_W;
           h = 16 * GRID_H;
         };
@@ -100,21 +36,22 @@ class A3A_RequestMissionDialog : A3A_DefaultDialog
         class ConquestButton : A3A_Button
         {
           idc = -1;
-          text = "Conquest"; // TODO: localize
+          text = $STR_antistasi_dialogs_mission_request_conquest;
           sizeEx = GUI_TEXT_SIZE_LARGE;
-          x = 16 * GRID_W;
-          y = 70 * GRID_H;
+          x = 7 * GRID_W;
+          y = 34 * GRID_H;
           w = 32 * GRID_W;
           h = 12 * GRID_H;
         };
 
+        // Destroy
         class DestroyIcon : A3A_Picture
         {
           idc = -1;
           colorBackground[] = {0,0,0,0};
           text = A3A_Tex_Icon_Destroy;
-          x = 72 * GRID_W;
-          y = 50 * GRID_H;
+          x = 53 * GRID_W;
+          y = 14 * GRID_H;
           w = 16 * GRID_W;
           h = 16 * GRID_H;
         };
@@ -122,21 +59,67 @@ class A3A_RequestMissionDialog : A3A_DefaultDialog
         class DestroyButton : A3A_Button
         {
           idc = -1;
-          text = "Destroy"; // TODO: localize
+          text = $STR_antistasi_dialogs_mission_request_destroy;
           sizeEx = GUI_TEXT_SIZE_LARGE;
-          x = 64 * GRID_W;
-          y = 70 * GRID_H;
+          x = 45 * GRID_W;
+          y = 34 * GRID_H;
           w = 32 * GRID_W;
           h = 12 * GRID_H;
         };
 
+        // Assasination
+        class AssassinationIcon : A3A_Picture
+        {
+          idc = -1;
+          text = A3A_Tex_Icon_Assassination;
+          x = 91 * GRID_W;
+          y = 14 * GRID_H;
+          w = 16 * GRID_W;
+          h = 16 * GRID_H;
+        };
+
+        class AssassinationButton : A3A_Button
+        {
+          idc = -1;
+          text = $STR_antistasi_dialogs_mission_request_assassination;
+          sizeEx = GUI_TEXT_SIZE_LARGE;
+          x = 83 * GRID_W;
+          y = 34 * GRID_H;
+          w = 32 * GRID_W;
+          h = 12 * GRID_H;
+        };
+
+        // Convoy
+        class ConvoyIcon : A3A_Picture
+        {
+          idc = -1;
+          colorBackground[] = {0,0,0,0};
+          text = A3A_Tex_Icon_Convoy_Ambush;
+          x = 129 * GRID_W;
+          y = 14 * GRID_H;
+          w = 16 * GRID_W;
+          h = 16 * GRID_H;
+        };
+
+        class ConvoyButton : A3A_Button
+        {
+          idc = -1;
+          text = $STR_antistasi_dialogs_mission_request_convoy;
+          sizeEx = GUI_TEXT_SIZE_LARGE;
+          x = 121 * GRID_W;
+          y = 34 * GRID_H;
+          w = 32 * GRID_W;
+          h = 12 * GRID_H;
+        };
+
+        // Rescue
         class RescueIcon : A3A_Picture
         {
           idc = -1;
           colorBackground[] = {0,0,0,0};
           text = A3A_Tex_Icon_Rescue;
-          x = 120 * GRID_W;
-          y = 50 * GRID_H;
+          x = 34 * GRID_W;
+          y = 56 * GRID_H;
           w = 16 * GRID_W;
           h = 16 * GRID_H;
         };
@@ -144,10 +127,56 @@ class A3A_RequestMissionDialog : A3A_DefaultDialog
         class RescueButton : A3A_Button
         {
           idc = -1;
-          text = "Rescue"; // TODO: localize
+          text = $STR_antistasi_dialogs_mission_request_rescue;
           sizeEx = GUI_TEXT_SIZE_LARGE;
-          x = 112 * GRID_W;
-          y = 70 * GRID_H;
+          x = 26 * GRID_W;
+          y = 76 * GRID_H;
+          w = 32 * GRID_W;
+          h = 12 * GRID_H;
+        };
+
+        // Logistics
+        class LogisticsIcon : A3A_Picture
+        {
+          idc = -1;
+          colorBackground[] = {0,0,0,0};
+          text = A3A_Tex_Icon_Logistics;
+          x = 72 * GRID_W;
+          y = 56 * GRID_H;
+          w = 16 * GRID_W;
+          h = 16 * GRID_H;
+        };
+
+        class LogisticsButton : A3A_Button
+        {
+          idc = -1;
+          text = $STR_antistasi_dialogs_mission_request_logistics;
+          sizeEx = GUI_TEXT_SIZE_LARGE;
+          x = 64 * GRID_W;
+          y = 76 * GRID_H;
+          w = 32 * GRID_W;
+          h = 12 * GRID_H;
+        };
+
+        // Support
+        class SupportIcon : A3A_Picture
+        {
+          idc = -1;
+          colorBackground[] = {0,0,0,0};
+          text = A3A_Tex_Icon_Logistics; // TODO: Make new icon
+          x = 110 * GRID_W;
+          y = 56 * GRID_H;
+          w = 16 * GRID_W;
+          h = 16 * GRID_H;
+        };
+
+        class SupportButton : A3A_Button
+        {
+          idc = -1;
+          text = $STR_antistasi_dialogs_mission_request_support;
+          sizeEx = GUI_TEXT_SIZE_LARGE;
+          x = 102 * GRID_W;
+          y = 76 * GRID_H;
           w = 32 * GRID_W;
           h = 12 * GRID_H;
         };
